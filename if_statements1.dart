2fc81b9c3f63b8bcc60e314f44048404dd297790
Sara Patel <patelsara839@gmail.com>
2025-06-06 11:59:49 +0530

    if statements

---
+++ if_statements1.dart
@@ -0,0 +1,22 @@
+// check whether n is positive
+
+
+
+void main()
+{
+  int n=10;
+
+  if(n>0)
+  {
+    print("number is positive");
+  }
+  else if(n==0)
+  {
+    print("number is zero");
+  }
+  else 
+  {
+    print("number is negative");
+  }
+}
+
---
+++ if_statements2.dart
@@ -0,0 +1,24 @@
+// Grading system
+
+ main() {
+  int score = 70;
+
+  if (score >= 90)
+   {
+    print("Grade A");
+   } 
+  else if (score >= 80) 
+   {
+    print("Grade B");
+   } 
+  
+   else if (score >= 70)
+   {
+    print("Grade C");
+   } 
+   else 
+   {
+    print(" Fail");
+   }
+}
+
