8ca283ec0f7a7ca6094e70284c2260e927fae393
Sara Patel <patelsara839@gmail.com>
2025-06-10 12:14:53 +0530

    commit

---
+++ switch_case.dart
@@ -0,0 +1,24 @@
+void main()
+{
+   String day= 'Monday';
+
+   switch(day)
+   {
+    case 'Monday' :
+    {
+      print("towards a new start");
+      break;
+    }
+    case 'Thursday' :
+    {
+      print("Middle of the week");
+      break;
+    }
+    case 'Friday' :
+    {
+      print("weekend!");
+    }
+
+
+   }
+}
